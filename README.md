b4ee135fb5f2d4641962ad63fa32f1c327cabdf5
studnik18 <maciejstudniarz@gmail.com>
2017-09-21 20:11:48 +0200

    handled initial choice of continents scope

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+World flags quiz is a game made with ReactJS where You are expected to guess from within 3 answers what flag is shown. It's upgraded version of 'Guess the flag' written with help of jQuery.
+Project under construction.
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 Below you will find some information on how to perform common tasks.<br>
--- src/App.js
+++ src/App.js
@@ -13,54 +13,76 @@ class App extends Component {
 
 
     constructor(props) {
-    super(props);
+        super(props);
 
-    this.state = {
-      turn: 0,
-      maxTurns: 0,
-      score: 0,
-      countriesArray: [],
-      answerIndex: '',
-      gameInProgress: false
-    };
+        this.state = {
+            turn: 0,
+            maxTurns: 0,
+            score: 0,
+            countriesArray: [],
+            answerIndex: '',
+            gameInProgress: false
+        };
 
-    this.startGame = this.startGame.bind(this);
+        this.startGame = this.startGame.bind(this);
+        this.prepareCountriesArray = this.prepareCountriesArray.bind(this);
 
+    }
 
+    startGame() {
+
+        let turns = document.getElementById('turns');
+
+        this.setState({
+            gameInProgress: true,
+            maxTurns: turns[0].checked ? 5 : 10,
+            countriesArray: this.prepareCountriesArray()
+        });
+
+    }
+
+    prepareCountriesArray() {
+        let continentsForm = document.getElementById('continents');
+        let continentsFormArray = Array.prototype.slice.call(continentsForm.elements);
+        let continentsNames = [];
+
+        continentsFormArray.map(continent => {
+            if (continent.checked) {
+                continentsNames.push(continent.name);
+            }
+            return continentsNames;
+        });
+
+        let filterCountries = (country) => {
+            return  country.continent === continentsNames[0] || country.continent === continentsNames[1] ||
+                    country.continent === continentsNames[2] || country.continent === continentsNames[3] ||
+                    country.continent === continentsNames[4] || country.continent === continentsNames[5]
+        };
+
+        return flagData.filter(filterCountries);
+    }
+
+    render() {
+        console.log(this.state);
+            return (
+                <div>
+                    <div className="App">
+                        <div className="App-header">
+                            <img src={logo} className="App-logo" alt="logo" />
+                            <h2>Welcome to React</h2>
+                        </div>
+                        <p className="App-intro">
+                        To get started, edit <code>src/App.js</code> and save to reload.
+                        </p>
+                    </div>
+                    { this.state.gameInProgress ? <FlagsMainContainer /> : 
+                        <SelectContinents handleClick={this.startGame}/> }
+                
+                
+                </div>
+        );
+      }
   }
-
-
-  startGame() {
-    
-    let turns = document.getElementById('turns');
-
-    this.setState({
-      gameInProgress: true,
-      maxTurns: turns[0].checked ? 5 : 10
-    });
-
-  }
-
-  render() {
-    console.log(this.state);
-    return (
-      <div>
-        <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-          </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
-        { this.state.gameInProgress ? <FlagsMainContainer /> : <SelectContinents handleClick={this.startGame}/> }
-        
-        
-      </div>
-    );
-  }
-}
 
 export default App;
 
